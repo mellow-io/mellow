07054bf7ded8fef9e8d9a8e975434370e00eee9e
a86913179 <a86913179@gmail.com>
2020-04-03 07:35:09 +0800

    Update config_route.bat
    Use PowerShell to modify route table

--- src/helper/win32/config_route.bat
+++ src/helper/win32/config_route.bat
@@ -1,7 +1,10 @@
 set TUN_GW=%1
 set ORIG_GW=%2
+for /f %%a in ('PowerShell -Command "& {Get-NetAdapter -InterfaceDescription *TAP* | Select-Object -ExpandProperty InterfaceAlias}"') do (set TAP=%%a)
+for /f %%b in ('PowerShell -Command "& {Get-NetRoute -DestinationPrefix 0.0.0.0/0 -InterfaceMetric 25 | Select-Object -ExpandProperty InterfaceAlias}"') do (set WAN=%%b)
 
-route delete 0.0.0.0
-route add 0.0.0.0 mask 0.0.0.0 %TUN_GW% metric 2
-route add 0.0.0.0 mask 0.0.0.0 %ORIG_GW% metric 200
-ipconfig /flushdns
+PowerShell -Command "& {Remove-NetRoute -DestinationPrefix 0.0.0.0/0 -Confirm:$False}"
+PowerShell -Command "& {New-NetRoute -DestinationPrefix 0.0.0.0/0 -InterfaceAlias %TAP% -NextHop %TUN_GW% -RouteMetric 0 -PolicyStore ActiveStore -Confirm:$False}"
+PowerShell -Command "& {New-NetRoute -DestinationPrefix 0.0.0.0/0 -InterfaceAlias %WAN% -NextHop %ORIG_GW% -RouteMetric 0 -PolicyStore ActiveStore -Confirm:$False}"
+PowerShell -Command "& {Set-NetIPInterface -InterfaceAlias %WAN% -AddressFamily IPv6 -RouterDiscovery Disabled}"
+PowerShell -Command "& {Clear-DnsClientCache}"
