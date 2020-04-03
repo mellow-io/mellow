2ff4f87fff2d7cc7527625aec4b4d4e43bdd43ce
a86913179 <a86913179@gmail.com>
2020-04-04 04:09:25 +0800

    Use PowerShell to modify route & metric

--- src/helper/win32/ensure_tap_device.bat
+++ src/helper/win32/ensure_tap_device.bat
@@ -153,8 +153,11 @@ netsh interface set interface "%DEVICE_NAME%" admin=enabled
 ::   exit /b 1
 :: )
 
-echo Set TAP adapter metric to 2.
-netsh interface ip set interface %DEVICE_NAME% metric=2
+echo Set all adapters metric to auto.
+PowerShell -Command "& {Set-NetIPInterface -InterfaceAlias * -AutomaticMetric Enabled}"
+
+echo Set TAP adapter metric to 0.
+PowerShell -Command "& {Set-NetIPInterface -InterfaceAlias %DEVICE_NAME% -InterfaceMetric 0}"
 
 echo TAP network device added successfully.
 
