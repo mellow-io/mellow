4a291e09abe397dadcec7eb810c584e7ce0e7921
a86913179 <a86913179@gmail.com>
2020-04-05 04:02:13 +0800

    Change win32 batch file input params

--- src/helper/win32/config_route.bat
+++ src/helper/win32/config_route.bat
@@ -1,6 +1,6 @@
 set TUN_GW=%1
-for /f %%a in ('PowerShell -Command "& {Get-NetAdapter -InterfaceDescription *TAP* | Select-Object -ExpandProperty InterfaceAlias}"') do (set TAP=%%a)
+set DEVICE_NAME=%2
 
-PowerShell -Command "& {New-NetRoute -DestinationPrefix 0.0.0.0/0 -InterfaceAlias %TAP% -NextHop %TUN_GW% -RouteMetric 0 -PolicyStore ActiveStore -Confirm:$False}"
-PowerShell -Command "& {Set-NetIPInterface -InterfaceAlias * -AddressFamily IPv6 -RouterDiscovery Disabled}"
+PowerShell -Command "& {New-NetRoute -DestinationPrefix 0.0.0.0/0 -InterfaceAlias %DEVICE_NAME% -NextHop %TUN_GW% -RouteMetric 0 -PolicyStore ActiveStore -Confirm:$False}"
+PowerShell -Command "& {Set-NetIPInterface -AddressFamily IPv6 -RouterDiscovery Disabled}"
 PowerShell -Command "& {Clear-DnsClientCache}"
