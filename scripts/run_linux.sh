cd6496d42b3b86170da5ba5caa9434c5d4341c8d
eycorsican <eric.y.corsican@gmail.com>
2020-07-29 00:46:26 +0800

    Refine linux run script

--- scripts/run_linux.sh
+++ scripts/run_linux.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
+EXE_FILE=core
+CONFIG_FILE=cfg.json
+GEOSITE_FILE=geosite.dat
+GEOMMDB_FILE=geo.mmdb
+DNS1=8.8.8.8
+DNS2=8.8.4.4
+TUN_GW=10.255.0.1
+TUN_ADDR=10.255.0.2
+TUN_MASK=255.255.255.0
+ORIG_GW=`ip route get 1 | awk '{print $3;exit}'`
+ORIG_ST_SCOPE=`ip route get 1 | awk '{print $5;exit}'`
+ORIG_ST=`ip route get 1 | awk '{print $7;exit}'`
+
 if [ `id -u` -ne 0 ]; then
   echo 'Must run as root!'
   exit 1
 fi
-
-
-EXE_FILE=core
-CONFIG_FILE=cfg.json
 
 if [ ! -f "$EXE_FILE" ]; then
 	echo "Executable file not found!"
@@ -23,9 +32,6 @@ if [ ! -x "$EXE_FILE" ]; then
 	echo "Run 'chmod +x $EXE_FILE' first!"
 	exit 1
 fi
-
-GEOSITE_FILE=geosite.dat
-GEOMMDB_FILE=geo.mmdb
 
 function ask_data_file {
 	while true; do
@@ -46,39 +52,49 @@ if [ ! -f "$GEOMMDB_FILE" ]; then
 	ask_data_file $GEOMMDB_FILE
 fi
 
-TUN_GW=10.255.0.1
-TUN_ADDR=10.255.0.2
-TUN_MASK=255.255.255.0
-ORIG_GW=`ip route get 1 | awk '{print $3;exit}'`
-ORIG_ST_SCOPE=`ip route get 1 | awk '{print $5;exit}'`
-ORIG_ST=`ip route get 1 | awk '{print $7;exit}'`
-
-echo "Original send through address $ORIG_ST (It should be a valid IP address)"
-echo "Original send through scope $ORIG_ST_SCOPE (It should be a valid network interface name)"
-echo "Original gateway $ORIG_GW (It should be a valid IP address)"
+echo -e "Detected send through address: \e[92m$ORIG_ST\e[0m <------------- \e[91mMake sure this is an IP address, otherwise you're in trouble.\e[0m"
+echo -e "Detected send through scope: \e[92m$ORIG_ST_SCOPE\e[0m <--------- \e[91mThis should be a network interface name.\e[0m"
+echo -e "Detected original gateway: \e[92m$ORIG_GW\e[0m <----------------- \e[91mThis should be an IP address.\e[0m"
 
 if [ -f `which realpath` ]; then
   CONFIG_FULL_PATH=`realpath $CONFIG_FILE`
-  echo "Using config file $CONFIG_FULL_PATH"
+  echo -e "Using config file \e[92m$CONFIG_FULL_PATH\e[0m"
 fi
+
+config_dns() {
+	if [ ! -d "/tmp/mellow" ]; then
+		mkdir /tmp/mellow
+	fi
+	cp /etc/resolv.conf /tmp/mellow/resolv.conf
+	echo "nameserver $DNS1" > /etc/resolv.conf
+	echo "nameserver $DNS2" >> /etc/resolv.conf
+	echo -e "Using DNS \e[92m$DNS1, $DNS2\e[0m"
+}
+
+recover_dns() {
+	cp /tmp/mellow/resolv.conf /etc/resolv.conf
+	echo "DNS recovered."
+}
 
 config_route() {
   # Give some time for Mellow to open the TUN device.
   sleep 3
-  echo 'Setting up routing table...'
   ip route del default table main
   ip route add default via $TUN_GW table main
   ip route add default via $ORIG_GW dev $ORIG_ST_SCOPE table default
   ip rule add from $ORIG_ST table default
+  echo "Routing table is ready."
 }
 
 recover_route() {
-  echo 'Recovering routing table...'
   ip rule del from $ORIG_ST table default
   ip route del default table default
   ip route del default table main
   ip route add default via $ORIG_GW table main
+  echo "Routing table recovered."
 }
+
+config_dns
 
 # Configure the routing table in the background.
 config_route &
@@ -100,3 +116,5 @@ $EXE_FULL_PATH \
 
 # Recover the routing table after Mellow exits.
 recover_route
+
+recover_dns
